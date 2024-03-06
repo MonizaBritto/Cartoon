31e8ea74afcae1f2e2a9f2f48cddc718416ee8ed
MOnizaBritto <monyza2017@gmail.com>
2024-03-06 20:36:28 -0300

    Criação da Header

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import Header from "./Components/Header/header.jsx"
+import {createGlobalStyle} from "styled-components"
+
+const GlobalStyle = createGlobalStyle `
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
+`
+function App(){
+  return(
+    <>
+    <GlobalStyle/>
+    <Header/>
+    </>
+  )
+}
+
+export default App 
---
+++ src/Components/Header/HeaderStyle.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+
+export const StyleHeader = styled.header `
+    background-color: #EDD820;
+    display:flex;
+    justify-content:space-around;
+    align-items:center;
+    height:25vh;
+    width: 100vw;
+`
+
+export const Logo = styled.img `
+    width: 14rem;
+    height:4rem;
+`
+
+export const Nav = styled.nav `
+    width: 30vw;
+    display:flex;
+    justify-content:space-evenly;
+`
+export const Categoria = styled.div `
+   text-align:center;
+`
+export const Icone = styled.img `
+    width: 6rem;
+    height:6rem;
+`
